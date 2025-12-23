10e86a159a7b42e07b9d70d2c53df26e993435ca
Sanjay-2001 <sanjaydutta976@gmail.com>
2025-12-23 18:32:27 +0530

    game platform added

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ k.loadSprite("chemical-bg", "graphics/chemical-bg.png");
 k.loadSprite("platforms", "graphics/platforms.png");
 
 k.scene("game", () => {
+  let gameSpeed = 100;
+  k.loop(1, () => {
+    gameSpeed += 50;
+  });
   const bgPieceWidth = 2880;
   const bgPieces = [
     k.add([k.sprite("chemical-bg"), k.pos(0, 0), k.opacity(0.8), k.scale(1.5)]),
@@ -13,6 +17,11 @@ k.scene("game", () => {
       k.opacity(0.8),
       k.scale(1.5),
     ]),
+  ];
+  const platformWidth = 2560;
+  const platforms = [
+    k.add([k.sprite("platforms"), k.pos(0, 450), k.scale(2)]),
+    k.add([k.sprite("platforms"), k.pos(platformWidth, 450), k.scale(2)]),
   ];
 
   k.onUpdate(() => {
@@ -24,6 +33,18 @@ k.scene("game", () => {
 
     bgPieces[0].move(-100, 0);
     bgPieces[1].moveTo(bgPieces[0].pos.x + bgPieceWidth * 2, 0);
+
+    if (platforms[1].pos.x < 0) {
+      platforms[0].moveTo(
+        platforms[1].pos.x + platformWidth,
+        platforms[1].pos.y
+      );
+      const frontPlatform = platforms.shift();
+      if (frontPlatform) platforms.push(frontPlatform);
+    }
+
+    platforms[0].move(-gameSpeed, 0);
+    platforms[1].moveTo(platforms[0].pos.x + platformWidth, platforms[0].pos.y);
   });
 });
 
